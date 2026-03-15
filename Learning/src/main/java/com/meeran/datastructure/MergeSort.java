153bffa7bf002d62a7e3118bba15d5a62f458697
reachmemeeran <reachmemeeran@gmail.com>
2026-03-15 16:44:27 +0000

    Corrected sorting algorithms.

--- Learning/src/main/java/com/meeran/datastructure/MergeSort.java
+++ Learning/src/main/java/com/meeran/datastructure/MergeSort.java
@@ -3,55 +3,65 @@ package com.meeran.datastructure;
 import java.util.Arrays;
 
 public class MergeSort {
-	
-	private static int[] merge(int[] array1, int[] array2) {
+
+	//Merge sorted arrays
+	private static int[] merge_old(int[] array1, int[] array2) {
 		int[] combined = new int[array1.length + array2.length];
 		int index=0, i=0, j=0;
-		
-		while(i<array1.length && j<array2.length) {
-			if(array1[i]<array2[j]) {
-				combined[index]=array1[i];
-				i++;
-			}else {
-				combined[index]=array2[j];
-				j++;
-			}
-			index++;
+		while (i < array1.length && j < array2.length) {
+			if (array1[i] < array2[j]) combined[index++] = array1[i++];
+			else combined[index++] = array2[j++];
 		}
-		while(i<array1.length) {
-			combined[index] = array1[i];
-			index++;
-			i++;
-		}
-		while(j<array2.length) {
-			combined[index] = array2[j];
-			index++;
-			j++;
-		}
+		while (i < array1.length) combined[index++] = array1[i++];
+		while (j < array2.length) combined[index++] = array2[j++];
 		return combined;
 	}
 	
 	// Space Complexity O(n)
 	// Time Complexity O(n log n)
-	private static int[] mergeSort(int[] array) {
+	private static int[] mergeSort_old(int[] array) {
 		if(array.length==1) return array;
 		int midIndex = array.length/2;
-		int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
-		int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+		int[] left = mergeSort_old(Arrays.copyOfRange(array, 0, midIndex));
+		int[] right = mergeSort_old(Arrays.copyOfRange(array, midIndex, array.length));
 		
-		return merge(left,right);
+		return merge_old(left,right);
+	}
+
+	private static void mergeSort(int[] array) {
+		mergeSort(array, new int[array.length], 0, array.length - 1);
+	}
+
+	private static void mergeSort(int[] array, int[] temp, int left, int right) {
+		if (left >= right) return;
+		int midIndex = left + (right - left ) / 2;
+
+		mergeSort(array, temp, left, midIndex);
+		mergeSort(array, temp, midIndex + 1, right);
+
+		merge(array, temp, left, midIndex, right);
+	}
+
+	private static void merge(int[] array, int[] temp, int left, int midIndex, int right) {
+		int i = left, j = midIndex + 1, k = left;
+		while (i <= midIndex && j <= right) {
+			if (array[i] <= array[j]) temp[k++] = array[i++];
+			else temp[k++] = array[j++];
+		}
+		while (i <= midIndex) temp[k++] = array[i++];
+		while (j <= right) temp[k++] = array[j++];
+
+		for (int x = left; x <= right; x++) {
+			array[x] = temp[x];
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] array1 = {1,3,7,8};
-		int[] array2 = {2,4,5,6};
-		
-		System.out.println(Arrays.toString(merge(array1, array2)));
-		
-		int[] mergeArray = {1,3,8,7,2,4,6,5};
-		System.out.println(Arrays.toString(mergeSort(mergeArray)));
-		
+		int[] array = {1,3,8,7,2,4,6,5};
+
+		System.out.println("Array -> "+Arrays.toString(array));
+		mergeSort(array);
+		System.out.println("Sorted Array -> "+Arrays.toString(array));
 	}
 
 }
--- Learning/src/main/java/com/meeran/datastructure/QuickSort.java
+++ Learning/src/main/java/com/meeran/datastructure/QuickSort.java
@@ -3,50 +3,43 @@ package com.meeran.datastructure;
 import java.util.Arrays;
 
 public class QuickSort {
-	
-	private static int pivot(int[] array, int pivotIndex, int endIndex) {
-		int swapIndex = pivotIndex;
-		for(int i=pivotIndex+1; i<=endIndex; i++) {
-			if(array[i]<array[pivotIndex]) {
+
+	private static void quickSort(int[] array) {
+		quickSort(array, 0, array.length-1);
+	}
+
+	private static void quickSort(int[] array, int left, int right) {
+		if (left < right) {
+			int pivotIndex = partition(array, left, right);
+			quickSort(array, left, pivotIndex - 1);
+			quickSort(array, pivotIndex + 1, right);
+		}
+	}
+
+	private static int partition(int[] array, int left, int right) {
+		int pivot = array[left], swapIndex = left;
+
+		for (int i = left + 1; i <= right; i++) {
+			if (array[i] < pivot) {
 				swapIndex++;
 				swap(array, swapIndex, i);
 			}
 		}
-		swap(array, pivotIndex, swapIndex);
+		swap(array, swapIndex, left);
 		return swapIndex;
 	}
-	
-	private static void swap(int[] array, int firstIndex, int secondIndex) {
-		int temp = array[firstIndex];
-		array[firstIndex] = array[secondIndex];
-		array[secondIndex] = temp;
-	}
-	
-	private static void quickSortHelper(int[] array, int left, int right) {
-		if(left<right) {
-			int pivotIndex = pivot(array, left,right);
-			quickSortHelper(array, left, pivotIndex-1);
-			quickSortHelper(array, pivotIndex+1,right);
-		}
-	}
 
-	// Space Complexity - O(1)
-	// Time Complexity - O(n log n)   ==> Worst posibility is O(n) if its already sorted
-	private static void quickSort(int[] array) {
-		quickSortHelper(array, 0, array.length-1);
+	private static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] pivotArray = {4,6,1,7,3,2,5};
-		
-		System.out.println("index: "+pivot(pivotArray, 0, 6));
-		System.out.println(Arrays.toString(pivotArray));
-		
-		int[] quickArray = {4,6,1,7,3,2,5};
-		quickSort(quickArray);
-		
-		System.out.println(Arrays.toString(quickArray));
+		int[] array = {4,6,1,7,3,2,5};
+		System.out.println("Array - "+Arrays.toString(array));
+		quickSort(array);
+		System.out.println("Sorted Array - "+Arrays.toString(array));
 	}
 
 }
